8d1a3af366b287b906eb682cea663ba735500fc6
@dipiCABJ <chichocat@gmail.com>
2018-06-21 20:21:55 -0300

    Add files via upload

---
+++ TP4_Laboratorio2/TP_4/MainCorreo/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace MainCorreo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstEstadoEntregado = new System.Windows.Forms.ListBox();
+            this.lstEstadoEnViaje = new System.Windows.Forms.ListBox();
+            this.lstEstadoIngresado = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnMostrarTodos = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.mtxtTrackingID = new System.Windows.Forms.MaskedTextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtbMostrar = new System.Windows.Forms.RichTextBox();
+            this.cmsLstEntregado = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mostrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.cmsLstEntregado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "En viaje";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(625, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Entregado";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstEstadoEntregado);
+            this.groupBox1.Controls.Add(this.lstEstadoEnViaje);
+            this.groupBox1.Controls.Add(this.lstEstadoIngresado);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(868, 308);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Estados Paquetes";
+            // 
+            // lstEstadoEntregado
+            // 
+            this.lstEstadoEntregado.FormattingEnabled = true;
+            this.lstEstadoEntregado.ItemHeight = 16;
+            this.lstEstadoEntregado.Location = new System.Drawing.Point(609, 82);
+            this.lstEstadoEntregado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstEstadoEntregado.Name = "lstEstadoEntregado";
+            this.lstEstadoEntregado.Size = new System.Drawing.Size(248, 196);
+            this.lstEstadoEntregado.TabIndex = 5;
+            // 
+            // lstEstadoEnViaje
+            // 
+            this.lstEstadoEnViaje.FormattingEnabled = true;
+            this.lstEstadoEnViaje.ItemHeight = 16;
+            this.lstEstadoEnViaje.Location = new System.Drawing.Point(311, 82);
+            this.lstEstadoEnViaje.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstEstadoEnViaje.Name = "lstEstadoEnViaje";
+            this.lstEstadoEnViaje.Size = new System.Drawing.Size(248, 196);
+            this.lstEstadoEnViaje.TabIndex = 4;
+            // 
+            // lstEstadoIngresado
+            // 
+            this.lstEstadoIngresado.FormattingEnabled = true;
+            this.lstEstadoIngresado.ItemHeight = 16;
+            this.lstEstadoIngresado.Location = new System.Drawing.Point(12, 82);
+            this.lstEstadoIngresado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstEstadoIngresado.Name = "lstEstadoIngresado";
+            this.lstEstadoIngresado.Size = new System.Drawing.Size(248, 196);
+            this.lstEstadoIngresado.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnMostrarTodos);
+            this.groupBox2.Controls.Add(this.btnAgregar);
+            this.groupBox2.Controls.Add(this.mtxtTrackingID);
+            this.groupBox2.Controls.Add(this.txtDireccion);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(495, 340);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(385, 199);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Paquetes";
+            // 
+            // btnMostrarTodos
+            // 
+            this.btnMostrarTodos.Location = new System.Drawing.Point(260, 137);
+            this.btnMostrarTodos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMostrarTodos.Name = "btnMostrarTodos";
+            this.btnMostrarTodos.Size = new System.Drawing.Size(109, 47);
+            this.btnMostrarTodos.TabIndex = 11;
+            this.btnMostrarTodos.Text = "Mostrar Todos";
+            this.btnMostrarTodos.UseVisualStyleBackColor = true;
+            this.btnMostrarTodos.Click += new System.EventHandler(this.btnMostrarTodos_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(260, 73);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(109, 31);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // mtxtTrackingID
+            // 
+            this.mtxtTrackingID.Location = new System.Drawing.Point(25, 78);
+            this.mtxtTrackingID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mtxtTrackingID.Mask = "000-000-0000";
+            this.mtxtTrackingID.Name = "mtxtTrackingID";
+            this.mtxtTrackingID.Size = new System.Drawing.Size(191, 22);
+            this.mtxtTrackingID.TabIndex = 9;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(25, 159);
+            this.txtDireccion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(191, 22);
+            this.txtDireccion.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Direccion:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tracking ID:";
+            // 
+            // rtbMostrar
+            // 
+            this.rtbMostrar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.rtbMostrar.Location = new System.Drawing.Point(12, 340);
+            this.rtbMostrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rtbMostrar.Name = "rtbMostrar";
+            this.rtbMostrar.Size = new System.Drawing.Size(477, 198);
+            this.rtbMostrar.TabIndex = 5;
+            this.rtbMostrar.Text = "";
+            // 
+            // cmsLstEntregado
+            // 
+            this.cmsLstEntregado.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.cmsLstEntregado.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mostrarToolStripMenuItem});
+            this.cmsLstEntregado.Name = "cmsLstEntregado";
+            this.cmsLstEntregado.Size = new System.Drawing.Size(130, 28);
+            // 
+            // mostrarToolStripMenuItem
+            // 
+            this.mostrarToolStripMenuItem.Name = "mostrarToolStripMenuItem";
+            this.mostrarToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.mostrarToolStripMenuItem.Text = "Mostrar";
+            this.mostrarToolStripMenuItem.Click += new System.EventHandler(this.mostrarToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 551);
+            this.Controls.Add(this.rtbMostrar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Correo UTN Dipietro.Diego.2D";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.cmsLstEntregado.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstEstadoEntregado;
+        private System.Windows.Forms.ListBox lstEstadoEnViaje;
+        private System.Windows.Forms.ListBox lstEstadoIngresado;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox mtxtTrackingID;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtbMostrar;
+        private System.Windows.Forms.Button btnMostrarTodos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ContextMenuStrip cmsLstEntregado;
+        private System.Windows.Forms.ToolStripMenuItem mostrarToolStripMenuItem;
+    }
+}
+
---
+++ TP4_Laboratorio2/TP_4/MainCorreo/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace MainCorreo
+{
+    public partial class Form1 : Form
+    {
+        Correo correo = new Correo();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Creará un nuevo paquete y asociará al evento InformaEstado el método paq_InformaEstado.
+            Paquete paq = new Paquete(this.mtxtTrackingID.Text, this.txtDireccion.Text);
+            paq.InformaEstado += Paq_InformaEstado;
+
+            //Agregará el paquete al correo, controlando las excepciones que puedan derivar de dicha acción.
+            try
+            {
+                correo += paq;
+                this.ActualizarEstados();
+                paq.informaExcepcion += MostrarExcepcionSQL;
+            }
+            catch (TrackingIdRepetidoException tIdE)
+            {
+
+                MessageBox.Show(tIdE.Message, "Atención!!");
+            }
+            catch(SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message, "Atención!!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Atencion!!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lstEstadoEntregado.ContextMenuStrip = cmsLstEntregado;
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            correo.FinEntregas();
+        }
+
+        private void ActualizarEstados()
+        {
+            lstEstadoIngresado.Items.Clear();
+            lstEstadoEnViaje.Items.Clear();
+            lstEstadoEntregado.Items.Clear();
+            try
+            {
+                foreach (Paquete xItem in correo.Paquetes)
+                {
+                    switch (xItem.Estado)
+                    {
+                        case Paquete.EEstado.Ingresado:
+                            lstEstadoIngresado.Items.Add(xItem);
+                            break;
+
+                        case Paquete.EEstado.EnViaje:
+                            lstEstadoEnViaje.Items.Add(xItem);
+                            break;
+
+                        case Paquete.EEstado.Entregado:
+                            lstEstadoEntregado.Items.Add(xItem);
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message,"Atención!!");
+            }
+            
+
+        }
+
+        private void MostrarInformacion<T>(IMostrar<T> elemento)
+        {
+            if (!object.ReferenceEquals(elemento, null))
+            {
+                try
+                {
+                    if (elemento is Correo)
+                    {
+                        this.rtbMostrar.Text = ((Correo)elemento).MostrarDatos((IMostrar<List<Paquete>>)elemento);
+                        GuardarString.Guardar(((Correo)elemento).MostrarDatos((IMostrar<List<Paquete>>)elemento), "Salida.txt");
+
+                    }
+                    else
+                        if (elemento is Paquete)
+                        {
+                            this.rtbMostrar.Text = ((Paquete)elemento).ToString();
+                            GuardarString.Guardar(((Paquete)elemento).ToString(), "Salida.txt");
+                        }
+                }
+                catch (FileNotFoundException fnfExc)
+                {
+                    MessageBox.Show(fnfExc.Message,"Atención");
+                }
+                catch (IOException ioExc)
+                {
+
+                    MessageBox.Show(ioExc.Message, "Atención");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Atención");
+                }
+            }
+        }
+
+        private void btnMostrarTodos_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<List<Paquete>>((IMostrar<List<Paquete>>)correo);
+        }
+
+        private void Paq_InformaEstado(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                Paquete.DelegadoEstado d = new Paquete.DelegadoEstado(Paq_InformaEstado);
+                this.Invoke(d, new object[] { sender, e });
+            }
+            else
+            {
+                // Llamar al método }
+                this.ActualizarEstados();
+            }
+        }
+
+        private void mostrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<Paquete>((IMostrar<Paquete>)lstEstadoEntregado.SelectedItem);
+        }
+
+        private void MostrarExcepcionSQL(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Atención!!");
+        }
+    }
+}
